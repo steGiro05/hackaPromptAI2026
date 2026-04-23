f9ad6b0efc036211e2da33f1f8ac9d8742b66c62
Stefano <stefanogiroletti9@gmail.com>
2026-04-23 13:58:53 +0200

    fixed urls

--- mybackend/mybackend/urls.py
+++ mybackend/mybackend/urls.py
@@ -26,10 +26,6 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/events/', include('events.urls')),
-    # Auth endpoints
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    # Le tue API eventi
-    path('api/events', include('events.urls')),
 ]
